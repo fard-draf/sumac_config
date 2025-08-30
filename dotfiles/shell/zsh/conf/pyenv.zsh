6ad52b99223316c8799e639bd60425be5331538a
fard-draf <fard@ik.me>
2025-08-30 13:18:56 +0200

    chore: extraction de la conf shell vers un submodule

--- dotfiles/shell/zsh/conf/pyenv.zsh
+++
@@ -1,8 +0,0 @@
-
-# ===================================================
-#        INITIALISATION DYNAMIQUE DE PYENV
-# ===================================================
-if command -v pyenv &>/dev/null; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-fi
--- dotfiles/shell/zsh/conf/ts.zsh
+++
@@ -1,8 +0,0 @@
-
-# ======= ALIAS TYPESCRIPT =======
-alias tsb='tsc --build'
-alias tsw='tsc --watch'
-alias tsc-check='tsc --noEmit'
-
-
-alias ts-al="show_aliases_from_file $DOTFILES/shell/zsh/conf/ts.zsh"
